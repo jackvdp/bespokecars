63262981fa6b02dde814df7771ed470887cb24e3
jackvdp <jack@pumpymusic.co.uk>
2026-01-11 12:27:07 +0000

    Update cars for particular ids

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,17 @@ import { client } from '@/sanity/lib/client'
 import HomeContent from './HomeContent'
 
 async function getCars() {
+  const carIds = [
+    'ff74d446-31e5-4fdc-b39a-2c83741e596e',
+    '5f259e8a-5e3b-4849-b07b-a5dc72f90c5e',
+    '8c75ced5-17b3-45cb-a969-2bab53f10f5f',
+    '08998623-64f6-40f0-93e5-59c60b1b29d7',
+    '5bf96dbc-649f-4867-8888-9e0ca27fa536',
+    'cd7a87c8-703f-4a0a-b0b5-ae6ff7728dc8'
+  ]
+  
   return client.fetch(`
-    *[_type == "car" && defined(imageUrls[0])] | order(_createdAt desc) [0...6] {
+    *[_type == "car" && _id in $ids] {
       _id,
       name,
       slug,
@@ -11,7 +20,7 @@ async function getCars() {
       logoUrl,
       imageUrls
     }
-  `)
+  `, { ids: carIds })
 }
 
 export default async function Home() {
