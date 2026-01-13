b737b085d0a63d4a0326639364157c8b5d5cae87
jackvdp <jack@pumpymusic.co.uk>
2026-01-13 21:23:53 +0000

    change cars

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import HomeContent from './HomeContent'
 async function getCars() {
   const carIds = [
     'ff74d446-31e5-4fdc-b39a-2c83741e596e',
-    '5f259e8a-5e3b-4849-b07b-a5dc72f90c5e',
+    '928ec8fd-f7fb-402a-9a0b-e3138613d2bb',
     '8c75ced5-17b3-45cb-a969-2bab53f10f5f',
     '08998623-64f6-40f0-93e5-59c60b1b29d7',
-    '5bf96dbc-649f-4867-8888-9e0ca27fa536',
+    'fa92539e-775a-446e-89b7-2fc62d7db4cb',
     'cd7a87c8-703f-4a0a-b0b5-ae6ff7728dc8'
   ]
   
